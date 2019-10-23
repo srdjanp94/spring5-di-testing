ac35a6fc576c05861cebccdae666f5ff31f096de
Srdjan Pavicevic <srdjan.12@hotmail.com>
2019-10-24 00:23:46 +0200

    Use default spring profile

--- src/main/java/com/springframework/services/PrimaryGermanGreetingService.java
+++ src/main/java/com/springframework/services/PrimaryGermanGreetingService.java
@@ -5,12 +5,12 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 @Service
+@Profile("de")
 @Primary
-@Profile({"en", "default"})
-public class PrimaryGreetingService implements GreetingService
+public class PrimaryGermanGreetingService implements GreetingService
 {
     @Override
     public String sayGreeting() {
-        return "Hello - Say from the Primary service";
+        return "Grüße von diesem Service";
     }
 }
