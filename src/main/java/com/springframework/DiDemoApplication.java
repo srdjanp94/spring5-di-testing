bded0472e5e663e059565b6584fdc391f6b5e695
Srdjan Pavicevic <srdjan.12@hotmail.com>
2019-10-31 00:24:41 +0100

    Externalize properties from datasource.properties file with Value annotation, and use some environment variables to override values from data source

--- src/main/java/com/springframework/DiDemoApplication.java
+++ src/main/java/com/springframework/DiDemoApplication.java
@@ -4,6 +4,7 @@ import com.springframework.controllers.ConstructorInjectedController;
 import com.springframework.controllers.MyController;
 import com.springframework.controllers.PropertyInjectedController;
 import com.springframework.controllers.SetterInjectedController;
+import com.springframework.examplebeans.FakeDataSource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -16,10 +17,9 @@ public class DiDemoApplication {
 
         MyController controller = (MyController) ctx.getBean("myController");
 
-        System.out.println(controller.hello());
-        System.out.println(ctx.getBean(PropertyInjectedController.class).sayHello());
-        System.out.println(ctx.getBean(SetterInjectedController.class).sayHello());
-        System.out.println(ctx.getBean(ConstructorInjectedController.class).sayHello());
+        FakeDataSource fakeDataSource = (FakeDataSource) ctx.getBean(FakeDataSource.class);
+
+        System.out.println(fakeDataSource.getUser());
     }
 
 }
---
+++ src/main/java/com/springframework/config/PropertyConfig.java
@@ -0,0 +1,44 @@
+package com.springframework.config;
+
+import com.springframework.examplebeans.FakeDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.env.Environment;
+
+@Configuration
+@PropertySource("classpath:datasource.properties")
+public class PropertyConfig
+{
+    @Autowired
+    Environment env;
+
+    @Value("${com.username}")
+    String user;
+
+    @Value("${com.password}")
+    String password;
+
+    @Value("${com.dburl}")
+    String url;
+
+    @Bean
+    public FakeDataSource fakeDataSource()
+    {
+        FakeDataSource fakeDataSource = new FakeDataSource();
+        fakeDataSource.setUser(env.getProperty("USERNAME"));
+        fakeDataSource.setPassword(password);
+        fakeDataSource.setUrl(url);
+        return fakeDataSource;
+    }
+
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer properties()
+    {
+        PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer = new PropertySourcesPlaceholderConfigurer();
+        return propertySourcesPlaceholderConfigurer;
+    }
+}
