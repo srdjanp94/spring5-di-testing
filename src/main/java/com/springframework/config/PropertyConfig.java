bc76544fe8cf8dd6fdddaa5d577f8045acbc2233
Srdjan Pavicevic <srdjan.12@hotmail.com>
2019-10-31 00:46:44 +0100

    Use centralized Spring Boot application.properties

--- src/main/java/com/springframework/config/PropertyConfig.java
+++ src/main/java/com/springframework/config/PropertyConfig.java
@@ -2,25 +2,13 @@ package com.springframework.config;
 
 import com.springframework.examplebeans.FakeDataSource;
 import com.springframework.examplebeans.FakeJmsBroker;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.annotation.PropertySources;
-import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
-import org.springframework.core.env.Environment;
 
 @Configuration
-@PropertySources({
-        @PropertySource("classpath:datasource.properties"),
-        @PropertySource("classpath:jms.properties")
-})
 public class PropertyConfig
 {
-    @Autowired
-    Environment env;
-
     @Value("${com.username}")
     String user;
 
@@ -43,7 +31,7 @@ public class PropertyConfig
     public FakeDataSource fakeDataSource()
     {
         FakeDataSource fakeDataSource = new FakeDataSource();
-        fakeDataSource.setUser(env.getProperty("USERNAME"));
+        fakeDataSource.setUser(user);
         fakeDataSource.setPassword(password);
         fakeDataSource.setUrl(url);
         return fakeDataSource;
@@ -57,12 +45,5 @@ public class PropertyConfig
         jmsBroker.setPassword(jmsPassword);
         jmsBroker.setUrl(jmsUrl);
         return jmsBroker;
-    }
-
-    @Bean
-    public static PropertySourcesPlaceholderConfigurer properties()
-    {
-        PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer = new PropertySourcesPlaceholderConfigurer();
-        return propertySourcesPlaceholderConfigurer;
     }
 }
